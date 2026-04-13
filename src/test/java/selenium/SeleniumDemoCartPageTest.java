67ac00605d76a66c05081d5d445f7ea8ce264acd
pm <pa.1231.no@gmail.com>
2026-04-13 20:15:27 +0200

    Add cart test

--- src/test/java/selenium/SeleniumDemoCartPageTest.java
+++ src/test/java/selenium/SeleniumDemoCartPageTest.java
@@ -12,10 +12,14 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.Arrays;
+
 public class SeleniumDemoCartPageTest {
 
     private WebDriver webDriver;
     private final String SELENIUM_BASE_URL = "http://seleniumdemo.com/";
+    public static final String EXPECTED_REDIRECTED_SHOP_URL = "http://seleniumdemo.com/?post_type=product";
+    public static final String EXPECTED_REDIRECTED_CART_URL = "http://seleniumdemo.com/?page_id=5";
 
 
     @BeforeEach
@@ -51,15 +55,12 @@ public class SeleniumDemoCartPageTest {
     public void testAddingToCartOneElement() {
         // given
         webDriver.get(SELENIUM_BASE_URL);
-        String expectedRedirectedCartUrl = "http://seleniumdemo.com/?page_id=5";
-        String expectedRedirectedShopUrl = "http://seleniumdemo.com/?post_type=product";
 
         // when
         WebElement cartWebElement = webDriver.findElement(By.xpath("//span[text()='Shop']"));
         cartWebElement.click();
         String redirectedShopUrl = webDriver.getCurrentUrl();
-        WebElement button = webDriver.findElement(By.cssSelector("a[data-product_id='29']"));
-        button.click();
+        clickAddToCart(29);
         WebDriverWait wait = new WebDriverWait(webDriver, 3);
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("a[title='View cart']")));
         webDriver.findElement(By.cssSelector("a[title='View cart']")).click();
@@ -68,8 +69,42 @@ public class SeleniumDemoCartPageTest {
         WebElement product = webDriver.findElement(By.linkText("BDD Cucumber"));
 
         // then
-        Assertions.assertEquals(expectedRedirectedCartUrl, redirectedCartUrl);
-        Assertions.assertEquals(expectedRedirectedShopUrl, redirectedShopUrl);
+        Assertions.assertEquals(EXPECTED_REDIRECTED_CART_URL, redirectedCartUrl);
+        Assertions.assertEquals(EXPECTED_REDIRECTED_SHOP_URL, redirectedShopUrl);
         Assertions.assertEquals("BDD Cucumber", product.getText());
+    }
+
+    @Test
+    public void testAddingToCartAllElements() {
+        // given
+        webDriver.get(SELENIUM_BASE_URL);
+
+        // when
+        WebElement cartWebElement = webDriver.findElement(By.xpath("//span[text()='Shop']"));
+        cartWebElement.click();
+        String redirectedShopUrl = webDriver.getCurrentUrl();
+
+        Arrays.asList(29, 27, 8).forEach(this::clickAddToCart);
+        WebDriverWait wait = new WebDriverWait(webDriver, 3);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("a[title='View cart']")));
+        webDriver.findElement(By.cssSelector("a[title='View cart']")).click();
+        String redirectedCartUrl = webDriver.getCurrentUrl();
+
+        WebElement bdd_cucumber = webDriver.findElement(By.partialLinkText("Cucumber"));
+        WebElement git_basics = webDriver.findElement(By.partialLinkText("GIT"));
+        WebElement java_selenium = webDriver.findElement(By.partialLinkText("Java"));
+        WebElement price = webDriver.findElement(By.cssSelector("tr.order-total span.woocommerce-Price-amount"));
+
+        // then
+        Assertions.assertEquals(EXPECTED_REDIRECTED_CART_URL, redirectedCartUrl);
+        Assertions.assertEquals(EXPECTED_REDIRECTED_SHOP_URL, redirectedShopUrl);
+        Assertions.assertEquals("BDD Cucumber", bdd_cucumber.getText());
+        Assertions.assertEquals("GIT basics", git_basics.getText());
+        Assertions.assertEquals("Java Selenium WebDriver", java_selenium.getText());
+        Assertions.assertEquals("11,99 zł", price.getText());
+    }
+
+    private void clickAddToCart(int id) {
+        webDriver.findElement(By.cssSelector("a[data-product_id='" + id + "']")).click();
     }
 }
