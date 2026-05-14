41369c27f2beec81ed4f1b0d91a374688d207832
pm <pa.1231.no@gmail.com>
2026-05-14 16:57:12 +0200

    Refactor

--- src/test/java/selenium/cart/SeleniumDemoCartPageTest.java
+++ src/test/java/selenium/cart/SeleniumDemoCartPageTest.java
@@ -12,21 +12,29 @@ import java.time.Duration;
 import java.util.Arrays;
 
 public class SeleniumDemoCartPageTest extends BaseTest {
+    public static final By CART = By.xpath("//span[text()='Cart']");
+    public static final By SHOP = By.xpath("//span[text()='Shop']");
+    public static final By VIEW_CART = By.cssSelector("a[title='View cart']");
+    public static final By CART_EMPTY = By.className("cart-empty");
+    public static final By BDD_CUCUMBER = By.linkText("BDD Cucumber");
+    public static final By CUCUMBER = By.partialLinkText("Cucumber");
+    public static final By GIT = By.partialLinkText("GIT");
+    public static final By JAVA = By.partialLinkText("Java");
+    public static final By TOTAL_PRICE_AMOUNT = By.cssSelector("tr.order-total span.woocommerce-Price-amount");
 
     @Test
     public void testEmptyCart() {
         // given
         webDriver.get(SELENIUM_BASE_URL);
-        String expectedRedirectedUrl = "http://seleniumdemo.com/?page_id=5";
 
         // when
-        WebElement cartWebElement = webDriver.findElement(By.xpath("//span[text()='Cart']"));
+        WebElement cartWebElement = webDriver.findElement(CART);
         cartWebElement.click();
         String redirectedUrl = webDriver.getCurrentUrl();
-        WebElement emptyCart = webDriver.findElement(By.className("cart-empty"));
+        WebElement emptyCart = webDriver.findElement(CART_EMPTY);
 
         // then
-        Assertions.assertEquals(expectedRedirectedUrl, redirectedUrl);
+        Assertions.assertEquals(EXPECTED_REDIRECTED_CART_URL, redirectedUrl);
         Assertions.assertTrue(emptyCart.isDisplayed());
 
     }
@@ -37,16 +45,16 @@ public class SeleniumDemoCartPageTest extends BaseTest {
         webDriver.get(SELENIUM_BASE_URL);
 
         // when
-        WebElement cartWebElement = webDriver.findElement(By.xpath("//span[text()='Shop']"));
+        WebElement cartWebElement = webDriver.findElement(SHOP);
         cartWebElement.click();
         String redirectedShopUrl = webDriver.getCurrentUrl();
         clickAddToCart(29);
         WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(3));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("a[title='View cart']")));
-        webDriver.findElement(By.cssSelector("a[title='View cart']")).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(VIEW_CART));
+        webDriver.findElement(VIEW_CART).click();
         String redirectedCartUrl = webDriver.getCurrentUrl();
 
-        WebElement product = webDriver.findElement(By.linkText("BDD Cucumber"));
+        WebElement product = webDriver.findElement(BDD_CUCUMBER);
 
         // then
         Assertions.assertEquals(EXPECTED_REDIRECTED_CART_URL, redirectedCartUrl);
@@ -60,20 +68,20 @@ public class SeleniumDemoCartPageTest extends BaseTest {
         webDriver.get(SELENIUM_BASE_URL);
 
         // when
-        WebElement cartWebElement = webDriver.findElement(By.xpath("//span[text()='Shop']"));
+        WebElement cartWebElement = webDriver.findElement(SHOP);
         cartWebElement.click();
         String redirectedShopUrl = webDriver.getCurrentUrl();
 
         Arrays.asList(29, 27, 8).forEach(this::clickAddToCart);
         WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(3));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("a[title='View cart']")));
-        webDriver.findElement(By.cssSelector("a[title='View cart']")).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(CART));
+        webDriver.findElement(CART).click();
         String redirectedCartUrl = webDriver.getCurrentUrl();
 
-        WebElement bdd_cucumber = webDriver.findElement(By.partialLinkText("Cucumber"));
-        WebElement git_basics = webDriver.findElement(By.partialLinkText("GIT"));
-        WebElement java_selenium = webDriver.findElement(By.partialLinkText("Java"));
-        WebElement price = webDriver.findElement(By.cssSelector("tr.order-total span.woocommerce-Price-amount"));
+        WebElement bdd_cucumber = webDriver.findElement(CUCUMBER);
+        WebElement git_basics = webDriver.findElement(GIT);
+        WebElement java_selenium = webDriver.findElement(JAVA);
+        WebElement price = webDriver.findElement(TOTAL_PRICE_AMOUNT);
 
         // then
         Assertions.assertEquals(EXPECTED_REDIRECTED_CART_URL, redirectedCartUrl);
